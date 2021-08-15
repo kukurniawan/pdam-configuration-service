3e4accf8d3d05c80db308287b6aa353f36efaa7e
kurnia.wirawan <kurnia.wirawan@gmail.com>
2021-08-15 20:23:00 +0700

    setup company init data &  trx decorator

--- Pdam.Configuration.Service/DataContext/Extension.cs
+++ Pdam.Configuration.Service/DataContext/Extension.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace Pdam.Configuration.Service.DataContext
 {
@@ -13,6 +17,28 @@ namespace Pdam.Configuration.Service.DataContext
                 .CreateScope();
             using var context = serviceScope.ServiceProvider.GetService<ConfigContext>();
             context?.Database.Migrate();
+            return builder;
+        }
+        
+        public static IApplicationBuilder SetupInitData(this IApplicationBuilder builder)
+        {
+            using var serviceScope = builder.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope();
+            using var context = serviceScope.ServiceProvider.GetService<ConfigContext>();
+            if (context == null) return builder;
+            var value = File.ReadAllText("DataContext\\init-company.json");
+            var companies = JsonConvert.DeserializeObject<List<Company>>(value);
+            if (companies == null) return builder;
+            foreach (var company in from company in companies
+                let w = context.Companies.FirstOrDefault(c => c.CompanyCode == company.CompanyCode)
+                where w == null
+                select company)
+            {
+                context.Companies.Add(company);
+            }
+
+            context.SaveChanges();
             return builder;
         }
     }
--- Pdam.Configuration.Service/Startup.cs
+++ Pdam.Configuration.Service/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +14,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Pdam.Configuration.Service.DataContext;
+using Pdam.Configuration.Service.Infrastructures;
 
 namespace Pdam.Configuration.Service
 {
@@ -31,6 +33,7 @@ namespace Pdam.Configuration.Service
             services.AddControllers();
             services.AddDbContext<ConfigContext>(c =>
                 c.UseNpgsql(Environment.GetEnvironmentVariable("PdamConfigurationConnectionString") ?? string.Empty));
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(TransactionDecorator<,>));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "Pdam.Configuration.Service", Version = "v1"});
@@ -40,13 +43,15 @@ namespace Pdam.Configuration.Service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.MigrateDatabase();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pdam.Configuration.Service v1"));
+                app.SetupInitData();
             }
-            app.MigrateDatabase();
+           
             app.UseRouting();
 
             app.UseAuthorization();

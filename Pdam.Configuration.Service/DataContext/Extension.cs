6e209170c0d1629a8911b17f6afa97c078762c18
kurnia.wirawan <kurnia.wirawan@gmail.com>
2021-08-15 16:39:30 +0700

    add migrations & extention migration

--- Pdam.Configuration.Service/DataContext/Extension.cs
+++ Pdam.Configuration.Service/DataContext/Extension.cs
@@ -1,7 +1,19 @@
-﻿namespace Pdam.Configuration.Service.DataContext
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pdam.Configuration.Service.DataContext
 {
     public static class Extension
     {
-        
+        public static IApplicationBuilder MigrateDatabase(this IApplicationBuilder builder)
+        {
+            using var serviceScope = builder.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope();
+            using var context = serviceScope.ServiceProvider.GetService<ConfigContext>();
+            context?.Database.Migrate();
+            return builder;
+        }
     }
 }
---
+++ Pdam.Configuration.Service/Migrations/20210815093814_init.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pdam.Configuration.Service.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyCode = table.Column<string>(type: "text", nullable: false),
+                    CompanyLegalName = table.Column<string>(type: "text", nullable: true),
+                    CompanyName = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    CompanyWeb = table.Column<string>(type: "text", nullable: true),
+                    PaymentEndPoint = table.Column<string>(type: "text", nullable: true),
+                    Logo = table.Column<string>(type: "text", nullable: true),
+                    FinanceHead = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Branches",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CompanyCode = table.Column<string>(type: "text", nullable: true),
+                    BranchName = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Branches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Branches_Companies_CompanyCode",
+                        column: x => x.CompanyCode,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyCode",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Branches_BranchName_CompanyCode",
+                table: "Branches",
+                columns: new[] { "BranchName", "CompanyCode" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Branches_CompanyCode",
+                table: "Branches",
+                column: "CompanyCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Branches");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
